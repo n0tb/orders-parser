41511fbbebb51c0dc2f6512f48432bec196dcefc
Yagodkin Igor <yagodkinigor@yandex.ru>
2018-07-13 07:42:18 +1000

    Initial commit

---
+++ src/main/java/ordersparser/OrdersParserApplication.java
@@ -0,0 +1,12 @@
+package ordersparser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdersParserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrdersParserApplication.class, args);
+    }
+}
---
+++ src/test/java/ordersparser/OrdersParserApplicationTests.java
@@ -0,0 +1,16 @@
+package ordersparser;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OrdersParserApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
